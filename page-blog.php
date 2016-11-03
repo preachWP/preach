48fdae07f0eae04afcc91c21ad03835104543e52
Kelvin <kelvinkfli@gmail.com>
2016-11-03 00:20:39 -0400

    custom template created for blog page

---
+++ page-blog.php
@@ -0,0 +1,66 @@
+<?php /* Template Name: CustomBlog */ ?>
+<?php get_header(); ?>
+
+<div class="blog-header">
+   <div class="blog-header-wrapper">
+      <p class="blog-title-tagline">preach stories</p>
+      <h1 class="blog-title">blog</h1>
+   </div>
+</div>
+
+<div class="blog-translate-container">
+   <div class="blog-grid">
+      <div class="blog-grid-left">
+         <div class="blog-grid-left-top"></div>
+         <div class="blog-grid-left-bottom">
+            <div class="blog-grid-left-bottom-left"></div>
+            <div class="blog-grid-left-bottom-right"></div>
+         </div>
+      </div>
+      <div class="blog-grid-right">
+         <div class="blog-grid-right-top">
+            <div class="blog-grid-right-top-left"></div>
+            <div class="blog-grid-right-top-right"></div>
+         </div>
+         <div class="blog-grid-right-bottom"></div>
+      </div>
+   </div>
+   <ul class="blog-grid-links">
+      <li>
+         <div class="link-whole">
+            <div class="link-image">one</div>
+            <div class="link-text">sports</div>
+         </div>
+      </li>
+      <li>
+         <div class="link-whole">
+            <div class="link-image">two</div>
+            <div class="link-text">design</div>
+         </div>
+      </li>
+      <li>
+         <div class="link-whole">
+            <div class="link-image">three</div>
+            <div class="link-text">fashion</div>
+         </div>
+      </li>
+      <li>
+         <div class="link-whole">
+            <div class="link-image">four</div>
+            <div class="link-text">culture</div>
+         </div>
+      </li>
+   </ul>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
