74b46e756d80a74f4ec908ae6bd96c77c47519c6
Kelvin <kelvinkfli@gmail.com>
2016-11-03 18:26:17 -0400

    worked on blog page

--- page-blog.php
+++ page-blog.php
@@ -8,21 +8,138 @@
    </div>
 </div>
 
+<?php
+   $ids = array();
+   if ( have_posts() ) :
+       while ( have_posts() ) : the_post();
+          array_push( $ids, get_the_ID() );
+       endwhile;
+   endif;
+?>
+
 <div class="blog-translate-container">
    <div class="blog-grid">
       <div class="blog-grid-left">
-         <div class="blog-grid-left-top"></div>
+         <div class="blog-grid-left-top">
+            <div class="overlay">
+               <div class="overlay-top">
+                  <?php $date = get_the_date('F jS, Y', $ids[0]) ?>
+                  <p><?php echo $date ?></p>
+                  <i class="fa fa-heart"></i>
+               </div>
+               <div class="overlay-bottom">
+                  <div class="overlay-header">
+                     <?php $headline = get_field('hero-tagline', $ids[0]) ?>
+                     <p><?php echo $headline ?></p>
+                  </div>
+                  <div class="overlay-content">
+                     <?php $blogsum = get_field('post_summary', $ids[0]) ?>
+                     <p><?php echo $blogsum ?></p>
+                  </div>
+               </div>
+            </div>
+         </div>
          <div class="blog-grid-left-bottom">
-            <div class="blog-grid-left-bottom-left"></div>
-            <div class="blog-grid-left-bottom-right"></div>
+            <div class="blog-grid-left-bottom-left">
+               <div class="overlay">
+                  <div class="overlay-top">
+                     <?php $date = get_the_date('F jS, Y', $ids[1]) ?>
+                     <p><?php echo $date ?></p>
+                     <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="overlay-bottom">
+                     <div class="overlay-header">
+                        <?php $headline = get_field('hero-tagline', $ids[1]) ?>
+                        <p><?php echo $headline ?></p>
+                     </div>
+                     <div class="overlay-content">
+                        <?php $blogsum = get_field('post_summary', $ids[1]) ?>
+                        <p><?php echo $blogsum ?></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="blog-grid-left-bottom-right">
+               <div class="overlay">
+                  <div class="overlay-top">
+                     <?php $date = get_the_date('F jS, Y', $ids[2]) ?>
+                     <p><?php echo $date ?></p>
+                     <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="overlay-bottom">
+                     <div class="overlay-header">
+                        <?php $headline = get_field('hero-tagline', $ids[2]) ?>
+                        <p><?php echo $headline ?></p>
+                     </div>
+                     <div class="overlay-content">
+                        <?php $blogsum = get_field('post_summary', $ids[2]) ?>
+                        <p><?php echo $blogsum ?></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
       <div class="blog-grid-right">
          <div class="blog-grid-right-top">
-            <div class="blog-grid-right-top-left"></div>
-            <div class="blog-grid-right-top-right"></div>
+            <div class="blog-grid-right-top-left">
+               <div class="overlay">
+                  <div class="overlay-top">
+                     <?php $date = get_the_date('F jS, Y', $ids[3]) ?>
+                     <p><?php echo $date ?></p>
+                     <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="overlay-bottom">
+                     <div class="overlay-header">
+                        <?php $headline = get_field('hero-tagline', $ids[3]) ?>
+                        <p><?php echo $headline ?></p>
+                     </div>
+                     <div class="overlay-content">
+                        <?php $blogsum = get_field('post_summary', $ids[3]) ?>
+                        <p><?php echo $blogsum ?></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="blog-grid-right-top-right">
+               <div class="overlay">
+                  <div class="overlay-top">
+                     <?php $date = get_the_date('F jS, Y', $ids[4]) ?>
+                     <p><?php echo $date ?></p>
+                     <i class="fa fa-heart"></i>
+                  </div>
+                  <div class="overlay-bottom">
+                     <div class="overlay-header">
+                        <?php $headline = get_field('hero-tagline', $ids[4]) ?>
+                        <p><?php echo $headline ?></p>
+                     </div>
+                     <div class="overlay-content">
+                        <?php $blogsum = get_field('post_summary', $ids[4]) ?>
+                        <p><?php echo $blogsum ?></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
-         <div class="blog-grid-right-bottom"></div>
+         <div class="blog-grid-right-bottom">
+            <div class="overlay">
+               <div class="overlay-top">
+                  <?php $date = get_the_date('F jS, Y', $ids[5]) ?>
+                  <p><?php echo $date ?></p>
+                  <i class="fa fa-heart"></i>
+               </div>
+               <div class="overlay-bottom">
+                  <div class="overlay-header">
+                     <?php $headline = get_field('hero-tagline', $ids[5]) ?>
+                     <p><?php echo $headline ?></p>
+                  </div>
+                  <div class="overlay-content">
+                     <?php $blogsum = get_field('post_summary', $ids[5]) ?>
+                     <p><?php echo $blogsum ?></p>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
    </div>
    <ul class="blog-grid-links">
@@ -53,11 +170,14 @@
    </ul>
 </div>
 
+<section class=form-section>
+   <?php the_field('blog_form_title'); ?>
+   <?php echo do_shortcode( '[contact-form-7 id="44" title="Newsletter"]' ); ?>
+   <p>
+      <?php the_field('blog_form_slogan'); ?>
+   </p>
 
-
-
-
-
+</section>
 
 
 
